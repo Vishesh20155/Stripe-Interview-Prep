9243d3d3a037454a66f4569c5a5db74d57fdef7a
Vishesh Rangwani <vishesh20155@iiitd.ac.in>
2023-10-19 02:04:19 +0530

    split chanaged

--- split.py
+++ split.py
@@ -1,5 +1,8 @@
 input_string = "Apple, Banana; Mango-Orange"
 delimiters = [",", ";", "-"]
 
-result = input_string.split(delimiters)
+for delimiter in delimiters:
+    input_string = " ".join(input_string.split(delimiter))
+
+result = input_string.split()
 print(result)
